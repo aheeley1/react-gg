f63eeb2d4e069d4780bd6d6c1e04cc028b2f2b02
Adam Heeley <adamheeley@Adams-MacBook-Air.local>
2025-03-07 16:00:41 -0700

    add function

--- declare/index.js
+++ declare/index.js
@@ -2,12 +2,26 @@ function double(arr) {
     const results = [];
 
     for (let i = 0; i < arr.length;i++) {
-        arr.push(nums[i] * 2);
+        results.push(nums[i] * 2);
     }
 
     return results;
 }
 
+function add(arr) {
+    let result = 0;
+
+    for(let i = 0; i < arr.length; i++) {
+        result += arr[i];
+    }
+
+    return result;
+}
+
 const nums = [1,2,3,4,5];
+
 const doubles = double(nums);
 console.log(doubles);
+
+const sum = add(nums);
+console.log(sum);
